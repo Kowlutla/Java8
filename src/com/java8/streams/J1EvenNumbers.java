30a061553e1173044e398bc48e5d5c8f7cc9b124
kowlutla.swamy.mangali@oracle.com <kowlutla.swamy.mangali@oracle.com>
2024-07-17 23:08:08 +0530

    all the even numbers that exist in the list

---
+++ src/com/java8/streams/J1EvenNumbers.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.java8.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author KowlutlaSwamy
+ *
+ */
+public class J1EvenNumbers {
+
+	public static void main(String[] args) {
+		/**
+		 * Given a list of integers, find out all the even numbers that exist in
+		 * the list using Stream functions?
+		 */
+		List<Integer> list = Arrays.asList(10, 15, 8, 49, 25, 98, 32);
+		List<Integer> evenNumbers = list.stream().filter(num -> num % 2 == 0)
+				.collect(Collectors.toList());
+		System.out.println("Even Numbers: " + evenNumbers);
+	}
+}
